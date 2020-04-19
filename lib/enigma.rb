84e7ffd0c3cd3081d415288e28fb668118f3f5c9
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-04-19 12:47:00 -1000

    add encode method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -10,6 +10,19 @@ class Enigma
 		@key = key ||= generate_random_key
 		@date = date
 	end
+  def generate_random_key
+    rand(5 ** 5).to_s.rjust(5,'0')
+  end
 
+  def encode
+    new_key = Key.new(key)
+    new_key.create_keys
+    offset = Offset.new(date)
+    offset.create_offset
+    shift = Shift.new(new_key,offset)
+    shift.final_shift
+    starting_indexes = get_starting_indices
+    generate_encoded_message(starting_indexes,shift)
+  end
 
 end
