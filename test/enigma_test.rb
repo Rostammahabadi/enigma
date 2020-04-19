1549bce316354d1eeec1297f78cb788c1227b8a0
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-04-19 12:41:26 -1000

    add test for encoding

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -14,4 +14,13 @@ class EnigmaTest < MiniTest::Test
 		assert_instance_of Enigma, enigma
 	end
 
+  def test_it_can_encode
+
+		new_enigma = Enigma.new
+		new_enigma.encrypt("hello world!", "02715", "040895")
+		assert_equal "keder ohulw!", new_enigma.encode
+		new_enigma.encrypt("abcd", "01824", "190420")
+		assert_equal "hxda", new_enigma.encode
+	end
+
 end
