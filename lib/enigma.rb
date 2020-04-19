24bde1ade2aede788fb96f59836c76690affbb34
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-04-19 12:46:31 -1000

    add encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,4 +5,11 @@ class Enigma
 		@character_set = ("a".."z").to_a << " "
 	end
 
+  def encrypt(message, key, date= Date.today)
+		@message = message.downcase.split("")
+		@key = key ||= generate_random_key
+		@date = date
+	end
+
+
 end
