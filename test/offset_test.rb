337b6574e48c5f946f75f5cd0f7bede12f1877e2
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-04-19 12:37:50 -1000

    test it has attributes

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -10,4 +10,14 @@ class OffsetTest < MiniTest::Test
     assert_instance_of Offset, Offset.new('040895')
   end
 
+  def test_it_has_attributes
+    new_offset = Offset.new('040895')
+
+    assert_equal '040895', new_offset.date
+    assert_equal 0, new_offset.aoffset
+    assert_equal 0, new_offset.boffset
+    assert_equal 0, new_offset.coffset
+    assert_equal 0, new_offset.doffset
+  end
+
 end
