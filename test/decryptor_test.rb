fbcc9c5b2bf0d780f74ce10c810c423962269232
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-04-20 10:51:10 -1000

    add spacing in test for readbility

--- test/decryptor_test.rb
+++ test/decryptor_test.rb
@@ -21,6 +21,7 @@ class DecryptorTest < MiniTest::Test
   def test_it_has_attributes
 
     new_decryptor = Decryptor.new("keder ohulw!", "01824", "190420")
+
     assert_equal "keder ohulw!", new_decryptor.message
     assert_equal "01824", new_decryptor.key
     assert_equal "190420", new_decryptor.date
@@ -29,24 +30,24 @@ class DecryptorTest < MiniTest::Test
   end
 
   def test_it_can_get_shifts
-
     new_decryptor = Decryptor.new("keder ohulw!", "02715", "040895")
+
     assert_equal [3, 27, 73, 20], new_decryptor.get_shifts
   end
 
   def test_it_can_get_starting_indices
-
   new_decryptor = Decryptor.new("keder ohulw!", "02715", "040895")
   expected = [10, 4, 3, 4, 17, 26, 14, 7, 20, 11, 22, nil]
+
   assert_equal expected, new_decryptor.get_starting_indices
   end
 
   def test_it_can_generate_decoded_message
-
     new_decryptor = Decryptor.new("keder ohulw!", "02715", "040895")
     indexes = [10, 4, 3, 4, 17, 26, 14, 7, 20, 11, 22, nil]
     shifts = [3, 27, 73, 20]
     expected = {:decryption=>"hello world!", :date=>"040895", :key=>"02715"}
+
     assert_equal expected, new_decryptor.generate_decoded_message
   end
 
@@ -54,6 +55,7 @@ class DecryptorTest < MiniTest::Test
     new_decryptor = Decryptor.new("keder ohulw!", "02715", "040895")
     indexes = [10, 4, 3, 4, 17, 26, 14, 7, 20, 11, 22, nil]
     shifts = [3, 27, 73, 20]
+
     assert_equal "hello world!", new_decryptor.decoding(indexes,shifts)
   end
 
