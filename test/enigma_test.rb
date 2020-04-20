c0f1b54d6c0c7d91b4b52fd7bdbc1c47f807c743
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-04-19 14:11:05 -1000

    change format of tests from encode to encrypt

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -5,7 +5,9 @@ require './lib/offset'
 require './lib/shift'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha'
 require 'pry'
+require 'mocha/minitest'
 
 class EnigmaTest < MiniTest::Test
 
@@ -17,36 +19,46 @@ class EnigmaTest < MiniTest::Test
   def test_it_can_encode
 
 		new_enigma = Enigma.new
-		new_enigma.encrypt("hello world!", "02715", "040895")
-		assert_equal "keder ohulw!", new_enigma.encode
-		new_enigma.encrypt("abcd", "01824", "190420")
-		assert_equal "hxda", new_enigma.encode
+		#new_enigma.encrypt("hello world!", "02715", "040895")
+    expected = {:encryption=>"keder ohulw!", :date=>"040895", :key=>"02715"}
+		assert_equal expected, new_enigma.encrypt("hello world!", "02715", "040895")
+    expected = {:encryption=>"hxda", :date=>"190420", :key=>"01824"}
+		assert_equal expected, new_enigma.encrypt("abcd", "01824", "190420")
 	end
 
   def test_it_encodes_single_characters
 		new_enigma = Enigma.new
-		new_enigma.encrypt("A", "02715", "040895")
-		assert_equal "d", new_enigma.encode
+    expected = {:encryption=>"d", :date=>"040895", :key=>"02715"}
+		assert_equal expected, new_enigma.encrypt("A", "02715", "040895")
 	end
-
-  # def test_it_can_encrypt_with_key_and_date
-	# 	skip
-	# 	enigma = Enigma.new
-
-	# 	assert_equal ({encryption: "keder ohulw", key: "02715", date: "040895"}), enigma.encrypt("hello world", "02715", "040895")
-	# end
 
 	def test_it_can_decrypt_with_key_and_date
+
 		enigma = Enigma.new
-    enigma.decrypt("keder ohulw", "02715", "040895")
-		assert_equal "hello world", enigma.decode
+    expected = {:encryption=>"hello world", :date=>"040895", :key=>"02715"}
+		assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
 	end
 
-	# def test_it_can_encrypt_with_key_using_todays_date
-	# 	skip
-	# 	enigma = Enigma.new
+  def test_it_can_encrypt
+    enigma = Enigma.new
+    expected = {encryption: "vjqtbeaweqihssi", key: "08304", date: "291018"}
+    assert_equal expected, enigma.encrypt("hello world end", "08304", "291018")
+  end
 
-	# 	assert_equal "encryption hash here", encrypted = enigma.encrypt("hello world", "02715")
-	# end
+  def test_encrypt_a_message_with_a_key_using_todays_date
+    Date.stubs(:today).returns(Date.new(1995,8,4))
+    enigma = Enigma.new
+    expected = ({:encryption=>"keder ohulw", :date=>"040895", :key=>"02715"})
+    assert_equal expected, enigma.encrypt("hello world", "02715")
+  end
+
+  def test_encrypt_a_message_with_a_message_and_no_key_or_date
+    skip
+    Enigma.stubs(:rjust).returns("02715")
+    Date.stubs(:today).returns(Date.new(1995,8,4))
+    enigma = Enigma.new
+    enigma.encrypt("hello world")
+    assert_equal ({:encryption=>"keder ohulw", :date=>"040895", :key=>"02715"}), enigma.encode
+  end
 
 end
