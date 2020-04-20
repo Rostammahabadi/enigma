3066a77eb56282190048dee3479f79d305624d4a
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-04-20 11:06:07 -1000

    update to work with encrypty and decrypt by adding global accessible variables

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,9 +1,13 @@
 class Enigma
+	attr_reader :message, :date, :key
 
 	def initialize()
 	end
 
   def encrypt(message, key = generate_random_key, date = generate_date_today)
+		@key = key
+		@message = message
+		@date = date
 		new_encryptor = Encryptor.new(message, key, date)
 		new_encryptor.generate_encoded_message
 	end
